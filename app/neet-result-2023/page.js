812ae3d6c6afceb27a5eab704ca547afcd19b58d
ashutosh725 <ashutosharma50@gmail.com>
2024-04-12 23:20:43 +0530

    kk

--- app/neet-result-2023/page.js
+++ app/neet-result-2023/page.js
@@ -2,7 +2,7 @@ import Budget from "@/components/neet-result/Budget";
 import Neet from "@/components/neet-result/Neet";
 import Result from "@/components/neet-result/Result";
 import Study from "@/components/neet-result/Study";
-import Faq from "@/components/neet-result/faq";
+import Faq from "@/components/neet-result/Faq";
 
 
 export default function Page(){
